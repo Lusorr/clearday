567a73fff5dc360a11a1ca56980f650871b9e089
Lusorr <129409805+Lusorr@users.noreply.github.com>
2025-07-23 21:55:07 +0300

    Update indexjs.js

--- indexjs.js
+++ indexjs.js
@@ -49,11 +49,6 @@ const products = [
     },
     {
         name: "Фотозона",
-        image: "blue1",
-        price: "от 10 000"
-    },
-    {
-        name: "Фотозона",
         image: "blue2",
         price: "от 10 000"
     },
